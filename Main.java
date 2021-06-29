e9f9b6d001ed2ac69514f88e293a539ab4b8f4a3
SandraDolotova <84093934+SandraDolotova@users.noreply.github.com>
2021-06-29 11:06:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Homework;
+public class Main {
+    public static void main(String[] args) {
+        /*A scientific calculator
+It can be as console / terminal application or a with nice graphics.
+Constructor_overloading.Calculator should have a menu where user can select the different calculations possible e.g
+square root, Trigonometry, average, interest rate, etc;
+you decide what you would like to include when it comes to functionalities.
+*/
+
+        Calculator calculator = new Calculator();
+        calculator.getCalculation();
+
+
+    }
+}
